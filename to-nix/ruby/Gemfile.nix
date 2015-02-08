f09891d262b2a4b0fc51855096c400c6b41b723d
Vladimir Kirillov <proger@wilab.org.ua>
2015-02-08 18:18:40 +0800

    initial import for ruby stuff

---
+++ to-nix/ruby/Gemfile.nix
@@ -0,0 +1,44 @@
+[
+  {
+    name = "faraday";
+    version = "0.9.0";
+    url = "http://rubygems.org/downloads/faraday-0.9.0.gem";
+    sha256 = "86409d329defa4b3b3b5774fcc0bf9b135ef40c60f147dbfaee056238f47918f";
+  }
+  {
+    name = "faraday_middleware";
+    version = "0.9.1";
+    url = "http://rubygems.org/downloads/faraday_middleware-0.9.1.gem";
+    sha256 = "d8e8e74a29ed19f07553cc82ed3a57b1bda0e3c3ca5b478c4c3faec0799ecdce";
+  }
+  {
+    name = "json";
+    version = "1.8.1";
+    url = "http://rubygems.org/downloads/json-1.8.1.gem";
+    sha256 = "961bfbbfa9fda1e857e9c791e964e6664e0d43bf687b19669dfbc7cdbc5e0200";
+  }
+  {
+    name = "multipart-post";
+    version = "2.0.0";
+    url = "http://rubygems.org/downloads/multipart-post-2.0.0.gem";
+    sha256 = "3dc44e50d3df3d42da2b86272c568fd7b75c928d8af3cc5f9834e2e5d9586026";
+  }
+  {
+    name = "newrelic_plugin";
+    version = "1.3.1";
+    url = "http://rubygems.org/downloads/newrelic_plugin-1.3.1.gem";
+    sha256 = "539d679bcb63c9b3a1bfffed4f1dfc0d9585a62769a5a58d5e57a725ab40b56e";
+  }
+  {
+    name = "rabbitmq_manager";
+    version = "0.1.0";
+    url = "http://rubygems.org/downloads/rabbitmq_manager-0.1.0.gem";
+    sha256 = "6cd53a92e92e7f67ee099bf84b36f78999278c9e554420e95923f834ada4c40c";
+  }
+  {
+    name = "redis";
+    version = "3.2.0";
+    url = "http://rubygems.org/downloads/redis-3.2.0.gem";
+    sha256 = "06d6084706fb8f8a79f569f2f1d1c3c8ae4b5bf9e53c834d0fd31fa81b500682";
+  }
+]

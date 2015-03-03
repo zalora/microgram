d216e5fe4939a025f26e6c22381580306bca56c7
Vladimir Kirillov <proger@wilab.org.ua>
2015-03-03 09:15:40 +0700

    add nixos/signing-keys.nix

---
+++ nixos/signing-keys.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, lib, ... }:
+{
+  system.activationScripts.buildSigningKeys = ''
+    if [[ ! -f /etc/nix/signing-key.sec ]]; then
+      ${pkgs.openssh}/bin/ssh-keygen -b 4096 -N "" -f /etc/nix/signing-key.sec
+      rm /etc/nix/signing-key.sec.pub
+      ${pkgs.openssl}/bin/openssl rsa -in /etc/nix/signing-key.sec -pubout > /etc/nix/signing-key.pub
+      chmod a-rwx,g+r /etc/nix/signing-key.*
+      chown nobody:users /etc/nix/signing-key.*
+    fi
+  '';
+}

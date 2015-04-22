0fda94adf935f75b26f1c575710d1a0f65e67bfa
Rickard Nilsson <rickynils@gmail.com>
2015-04-22 09:46:23 +0200

    OK, this was a meaningless exercise.
    @proger you were right, these modules were already disabled. I should
    have tested this in a much better way than I did.

--- nixos/cloud-config.nix
+++ nixos/cloud-config.nix
@@ -19,10 +19,6 @@ in
   nix.readOnlyStore = true;
   nix.trustedBinaryCaches = [ "http://hydra.nixos.org" ];
 
-  services.nixosManual.enable = cloudDefault false;
-  security.polkit.enable = cloudDefault false;
-  services.udisks2.enable = cloudDefault false;
-
   services.openssh.enable = cloudDefault true;
   services.openssh.passwordAuthentication = cloudDefault false;
   services.openssh.challengeResponseAuthentication = cloudDefault false;

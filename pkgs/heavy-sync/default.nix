222e092fd572e1fb5f3e5034ac90df110cf3c745
Hoang Xuan Phu <phuhimself@phunehehe.net>
2015-05-19 18:22:59 +0800

    bump heavy-sync

--- pkgs/heavy-sync/default.nix
+++ pkgs/heavy-sync/default.nix
@@ -6,8 +6,8 @@ buildPythonPackage rec {
 
   src = fetchgit {
     url = "https://github.com/zalora/heavy-sync.git";
-    rev = "410b300bf19465c3a1e11c81fa04c8d8dade890a";
-    sha256 = "563d3913fb67a1817608523d1c5ae7c42d192f2dee44ee4a04e73fa829bb5d40";
+    rev = "47cfbb3d3d183849f66263fc988aae2ece49dc92";
+    sha256 = "285ccfbf0328fd3b57b672d35c7d7ef5b98e8e4b347f77bccb416988e06ffec1";
   };
 
   propagatedBuildInputs = [

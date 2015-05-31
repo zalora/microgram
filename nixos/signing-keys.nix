97a7abd5eddee49d714b0639df0a4b9e2726008f
Vladimir Kirillov <proger@wilab.org.ua>
2015-05-31 15:19:23 +0200

    signing-keys: make sure /etc/nix exists

--- nixos/signing-keys.nix
+++ nixos/signing-keys.nix
@@ -2,6 +2,7 @@
 {
   system.activationScripts.buildSigningKeys = ''
     if [[ ! -f /etc/nix/signing-key.sec ]]; then
+      mkdir -p /etc/nix
       ${pkgs.openssh}/bin/ssh-keygen -b 4096 -N "" -f /etc/nix/signing-key.sec
       rm /etc/nix/signing-key.sec.pub
       ${pkgs.openssl}/bin/openssl rsa -in /etc/nix/signing-key.sec -pubout > /etc/nix/signing-key.pub

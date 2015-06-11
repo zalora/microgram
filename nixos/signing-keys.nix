002ba3150835f1952d36d6aaaef5700193457ebd
Vladimir Kirillov <proger@wilab.org.ua>
2015-06-11 15:39:26 +0200

    signing-keys: run after users; this activation script used to fail on boot every time

--- nixos/signing-keys.nix
+++ nixos/signing-keys.nix
@@ -1,10 +1,11 @@
 { config, pkgs, lib, ... }:
 {
-  system.activationScripts.buildSigningKeys = ''
+  system.activationScripts.buildSigningKeys = lib.stringAfter ["users"] ''
     if [[ ! -f /etc/nix/signing-key.sec ]]; then
       mkdir -p /etc/nix
+      # ssh-keygen needs users to exist
       ${pkgs.openssh}/bin/ssh-keygen -b 4096 -N "" -f /etc/nix/signing-key.sec
-      rm /etc/nix/signing-key.sec.pub
+      rm -f /etc/nix/signing-key.sec.pub
       ${pkgs.openssl}/bin/openssl rsa -in /etc/nix/signing-key.sec -pubout > /etc/nix/signing-key.pub
       chmod a-rwx,g+r /etc/nix/signing-key.*
       chown nobody:users /etc/nix/signing-key.*

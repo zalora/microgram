0687c2c6b81a04b8fe234e48e31aa726cbb4ec01
Tomislav Viljetic <tv@zalora.com>
2015-07-03 12:55:19 +0200

    pkg get-user-data: init

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -145,6 +145,12 @@ in rec {
     '';
   };
 
+  get-user-data = pkgs.writeScriptBin "get-user-data" ''
+    #! /bin/sh
+    exec ${retry}/bin/retry \
+      ${pkgs.curl}/bin/curl -sf http://169.254.169.254/latest/user-data
+  '';
+
   go = pkgs.callPackage ./go/1.4.nix {};
 
   graphviz = pkgs.callPackage ./graphviz {};

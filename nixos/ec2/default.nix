493a7eb73fa0b57a550d21b78d3606ff6f80ccd7
Vladimir Kirillov <proger@wilab.org.ua>
2015-07-08 17:24:36 -0700

    locked out!

--- nixos/ec2/default.nix
+++ nixos/ec2/default.nix
@@ -19,6 +19,22 @@ in
     # from user data. We don't want that.
     systemd.services."fetch-ec2-data".script = mkForce (''
       ip route del blackhole 169.254.169.254/32 || true
+
+      # Don't download the SSH key if it has already been injected
+      # into the image (a Nova feature).
+      if ! [ -e /root/.ssh/authorized_keys ]; then
+          echo "obtaining SSH key..."
+          mkdir -m 0700 -p /root/.ssh
+          $wget http://169.254.169.254/1.0/meta-data/public-keys/0/openssh-key > /root/key.pub
+          if [ $? -eq 0 -a -e /root/key.pub ]; then
+              if ! grep -q -f /root/key.pub /root/.ssh/authorized_keys; then
+                  cat /root/key.pub >> /root/.ssh/authorized_keys
+                  echo "new key added to authorized_keys"
+              fi
+              chmod 600 /root/.ssh/authorized_keys
+              rm -f /root/key.pub
+          fi
+      fi
     '' + optionalString (!config.ec2.metadata) ("\n" + ''
       # Since the user data is sensitive, prevent it from being
       # accessed from now on.

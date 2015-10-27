6cc4a9d9b3342c0fccbfaa1701f192308dca9405
vi <me@vikramverma.com>
2015-10-27 21:16:51 +0000

    Revert sproxy to bob-auth.
    Partial revert of 9e6a35e8bf2adcf39131880f4187bfb420700659 which switched
    branches, I presume mistakenly.

--- pkgs/sproxy/default.nix
+++ pkgs/sproxy/default.nix
@@ -1,29 +1,27 @@
-{ mkDerivation, aeson, attoparsec, base, bytestring, connection
-, containers, crypto-random, data-default, fetchgit, hspec
-, http-conduit, http-kit, http-types, interpolatedstring-perl6
-, logging-facade, logsink, network, optparse-applicative
-, postgresql-simple, resource-pool, SHA, split, stdenv
-, string-conversions, time, tls, unix, utf8-string, wai, warp, x509
-, yaml
+{ mkDerivation, aeson, attoparsec, base, bytestring
+, case-insensitive, certificate, containers, crypto-random
+, data-default, fetchgit, hslogger, http-conduit, http-kit
+, http-types, interpolatedstring-perl6, mtl, network
+, optparse-applicative, safe, SHA, split, stdenv, string-conversions
+, time, tls, unix, unordered-containers, utf8-string, x509, yaml
 }:
 mkDerivation {
   pname = "sproxy";
-  version = "0.9.0";
+  version = "0.8.0";
   src = fetchgit {
     url = "https://github.com/zalora/sproxy.git";
-    sha256 = "42498ae6a6ebdb78b451c4c3eb4532e2bac69f34ecc3dc3c47d8687a8ce38ab3";
-    rev = "d039fcb31a86290b8b76e0a82e08628781646e69";
+    sha256 = "fbf706eb34225f99f04eadc87fd04128292f77b0de1b59ab3c976d67e7dd0b3b";
+    rev = "c3e999cf381483f6ff64612ff1c2eaff5ea375f2";
   };
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    aeson attoparsec base bytestring containers crypto-random
-    data-default http-conduit http-kit http-types
-    interpolatedstring-perl6 logging-facade logsink network
-    postgresql-simple resource-pool SHA split string-conversions time
-    tls unix utf8-string x509 yaml
+    aeson attoparsec base bytestring case-insensitive certificate
+    containers crypto-random data-default hslogger http-conduit
+    http-kit http-types interpolatedstring-perl6 mtl network safe SHA
+    split string-conversions time tls unix unordered-containers
+    utf8-string x509 yaml
   ];
   executableHaskellDepends = [ base optparse-applicative ];
-  doCheck = false;
   license = stdenv.lib.licenses.mit;
 }

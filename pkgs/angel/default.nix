71660a6ef9c0c9223d8bce10e7e526920d16dbb6
Vladimir Kirillov <proger@wilab.org.ua>
2015-11-27 19:15:27 +0200

    use our angel backports

--- pkgs/angel/default.nix
+++ pkgs/angel/default.nix
@@ -1,4 +1,4 @@
-{ mkDerivation, base, configurator, containers, fetchzip, hspec
+{ mkDerivation, base, configurator, containers, fetchgit, hspec
 , mtl, old-locale, process, stdenv, stm, text, time, unix
 , unordered-containers
 }:
@@ -6,9 +6,11 @@ mkDerivation {
   pname = "angel";
   # do not bump until https://github.com/MichaelXavier/Angel/issues/40 is fixed
   version = "0.5.1";
-  src = fetchzip {
-    url = "http://hackage.haskell.org/package/angel-0.5.1/angel-0.5.1.tar.gz";
-    sha256 = "1qa7kfaia429la645naixfns5i9s775p9vzvlpk0xpz8zs8zfxcw";
+  src = fetchgit {
+    url = "https://github.com/zalora/Angel.git";
+    # branch backports
+    rev = "ff7f2dfc08edeede9c9da23ac4e6aa9afd2a56cc";
+    sha256 = "0643829fa4378f22220759e17bab8422c50ec8fb15f9ad2474aa04322b0d9a3f";
   };
   patches = [ ./static.patch ./less-logs.patch ];
   isLibrary = false;

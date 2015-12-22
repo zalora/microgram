94797b0eb20eedd22353db6f61ff74e42ac3927a
Vladimir Kirillov <proger@wilab.org.ua>
2015-12-22 16:26:54 +0200

    dynomite 0.5.6

--- pkgs/dynomite/default.nix
+++ pkgs/dynomite/default.nix
@@ -1,12 +1,12 @@
 { fetchurl, autoreconfHook, stdenv, openssl }:
 
 stdenv.mkDerivation rec {
-  name = "dynomite-0.5.5";
+  name = "dynomite-0.5.6";
 
   buildInputs = [ autoreconfHook openssl ];
 
   src = fetchurl {
-    url = https://github.com/Netflix/dynomite/archive/v0.5.5.tar.gz;
-    sha256 = "1xpz4rg29a8j0z6fp02swxf405m4d1kfq4jh577197sjw6hwi6v5";
+    url = https://github.com/Netflix/dynomite/archive/v0.5.6.tar.gz;
+    sha256 = "1jim17bg414lc4zd007q17hfbpgq8qgqafi06s3p746rzxc0iy6z";
   };
 }

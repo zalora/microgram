aa62810b2c964d94088f6423595add0cde33f07e
Vladimir Kirillov <proger@wilab.org.ua>
2015-12-23 12:49:56 +0200

    dynomite: no optimizations and stripping + keep debug info

--- pkgs/dynomite/default.nix
+++ pkgs/dynomite/default.nix
@@ -5,6 +5,15 @@ stdenv.mkDerivation rec {
 
   buildInputs = [ autoreconfHook openssl ];
 
+  dontStrip = true;
+
+  # See https://github.com/Netflix/dynomite/pull/223
+  # Keep debug info in case there are more crashes.
+  NIX_CFLAGS_COMPILE = [
+    "-O0"
+    "-ggdb"
+  ];
+
   src = fetchurl {
     url = https://github.com/Netflix/dynomite/archive/v0.5.6.tar.gz;
     sha256 = "1jim17bg414lc4zd007q17hfbpgq8qgqafi06s3p746rzxc0iy6z";

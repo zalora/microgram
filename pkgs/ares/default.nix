505ec02a991620ec349ad68b0f6615b371679f88
tv <tv@zalora.com>
2016-01-11 22:40:21 +0100

    ares: 0 -> 1

--- pkgs/ares/default.nix
+++ pkgs/ares/default.nix
@@ -1,23 +1,23 @@
 { fetchgit, mkDerivation, aeson, attoparsec, base, bytestring, containers
 , directory, extra, filelock, filepath, network, process
 , servant-server, stdenv, temporary, text, transformers, Unique
-, unix, wai, wai-extra, warp, linux-mount
+, unix, wai, wai-extra, warp
 }:
 mkDerivation {
   pname = "ares";
-  version = "0";
+  version = "1";
   patches = [ ./ug.patch ];
   src = fetchgit {
     url = https://github.com/zalora/ares;
-    rev = "c29c837efa0521445aeaadbd5c4bb1d932d034af";
-    sha256 = "28bd5b3513ef571314b775319c9d69f59809ebfa74fed3447b79df1a70d0aac7";
+    rev = "ff27e1ba950f40d5a08a6976abb4a2e532a57da9";
+    sha256 = "118c0f4dfc6955039e07d7d1c27f876fd0ae10ab2301c0a1c2ddfc380d77130b";
   };
   isLibrary = false;
   isExecutable = true;
   executableHaskellDepends = [
     aeson attoparsec base bytestring containers directory extra
     filelock filepath network process servant-server temporary text
-    transformers Unique unix wai wai-extra warp linux-mount
+    transformers Unique unix wai wai-extra warp
   ];
   license = stdenv.lib.licenses.bsd3;
 }

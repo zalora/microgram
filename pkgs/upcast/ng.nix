b29a4ccec878f37150de62d75330bfa588bec925
Vladimir Kirillov <proger@wilab.org.ua>
2016-03-23 19:18:34 +0200

    bump upcast-ng

--- pkgs/upcast/ng.nix
+++ pkgs/upcast/ng.nix
@@ -13,8 +13,8 @@ mkDerivation {
   version = "0.1.1.0";
   src = fetchgit {
     url = git://github.com/zalora/upcast.git;
-    rev = "c8faa2442bc840b7cb2f15d02e113295a6361df8";
-    sha256 = "4a8facdb8a564c3a263a1ae1a673624a71560b994f3876fa00b1ace7eaa35cff";
+    rev = "eac3b8c89e69d55d566f59f30e0226926381766d";
+    sha256 = "ded7efa4a43915e656ca8117b1d01257f03eba9c1b8197d5c627efe1b5159667";
   };
   isLibrary = true;
   isExecutable = true;
